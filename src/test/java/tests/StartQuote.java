65c61a00c6594ecff6b69a91ab90212db6ecaf37
Justin M <boogie@Justins-MacBook-Air.local>
2026-06-04 01:36:42 -0400

    first commit

---
+++ src/test/java/tests/StartQuote.java
@@ -0,0 +1,25 @@
+package tests;
+
+import base.BaseTest;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
+import org.testng.annotations.Test;
+
+public class WebsiteNavigation extends BaseTest {
+    @Test
+    public void googleSearch() {
+        page.navigate("https://google.com");
+        page.context().cookies();
+        page.getByRole(AriaRole.COMBOBOX).fill("Florida Home Insurance");
+        page.keyboard().press("Enter");
+        page.waitForLoadState(LoadState.NETWORKIDLE);
+        page.getByText("Delray Beach Home Insurance").click();
+
+
+
+
+        page.waitForLoadState(LoadState.NETWORKIDLE);
+
+    }
+
+}
